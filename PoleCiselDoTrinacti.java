7f54d0a46e18bf70fcc0aed9f6ddac492f4650e2
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-01 09:11:54 +0100

    Add files via upload

---
+++ PoleCiselDoTrinacti.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poleciseldotrinacti;
+
+/**
+ *
+ * @author 2021-e-hermankova
+ */
+public class PoleCiselDoTrinacti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] pole;
+        pole = new int[13];
+        for (int i = 0; i < 13; i++) {
+            pole[i] = i+1;
+        }
+        for (int i : pole) {
+            System.out.print(i + " ");
+        }
+    }
+}    
+ 
+        
+        
+        
+        
+        
+        
+   
+    
+    
+
